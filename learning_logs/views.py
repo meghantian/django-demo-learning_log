b2cbecb880c429afd6f2ecdbf2493fd573c50a88
Hannah Tian <menghantin@gmail.com>
2024-12-02 10:31:05 -0600

    update user data page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
+@login_required
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
